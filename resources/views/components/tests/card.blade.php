996b2410fc4a4ec98cc6c8ae1fd523fce1439813
ricardo <nishimu75618094@gmail.com>
2022-06-05 00:43:31 +0900

    com

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,9 @@
-<div class="border-2 shadow-md w-1/4 p2">
+@props([
+    'title' => 'タイトルです',
+    'message' => '初期値です',
+    'content' => '本文です'
+])
+<div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title}}</div>
     <div>画像</div>
     <div>{{ $content}}</div>
